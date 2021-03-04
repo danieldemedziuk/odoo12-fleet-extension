f186967a33782fce62f615f222b20e8ddb84558e
danieldemedziuk <daniel.demedziuk@gmail.com>
2021-03-04 11:34:26 +0100

    added help to specific fields

--- models/fleet_extension.py
+++ models/fleet_extension.py
@@ -15,11 +15,11 @@ class fleet_extension_model(models.Model):
     insurance_date = fields.Date(string='End date of insurance', compute='set_insurance_info', readonly=True)
     insurance_number = fields.Char(string='Insurance policy number')
     wheel_size = fields.Char(string='Wheel size')
-    fire_extinguisher_date = fields.Date(string='Expiration date of fire extinguisher')
+    fire_extinguisher_date = fields.Date(string='Expiration date of fire extinguisher', help='Does it exist in the car.')
     technical_review = fields.Date(string='Date of technical inspection')
-    spare_wheel = fields.Boolean(string='Spare wheel')
-    pin_card = fields.Integer(string='Card PIN')
-    pressure_sensor = fields.Boolean(string='Pressure sensor')
+    spare_wheel = fields.Boolean(string='Spare wheel', help='Does it exist in the car.')
+    pin_card = fields.Integer(string='Card PIN', help="This is the pin code for the company's fleet card.")
+    pressure_sensor = fields.Boolean(string='Pressure sensor', help='Does it exist in the car.')
     vignette = fields.Char(string='Vignette')
     vignette_date = fields.Date(string='Expiration date of the vignette')
 
@@ -43,8 +43,8 @@ class fleet_extension_model(models.Model):
                     'category': 'contract'
                 })
 
-            veh_log = self.env['fleet.vehicle.log.contract'].search([('active', '=', True), ('cost_subtype_id', '=', 'Insurance'), ('state', '=', 'open'), ('vehicle_id', '=', rec.name)])
+            veh_ins_log = self.env['fleet.vehicle.log.contract'].search([('active', '=', True), ('cost_subtype_id', '=', 'Insurance'), ('state', '=', 'open'), ('vehicle_id', '=', rec.name)])
 
-            if len(veh_log) == 1:
-                rec.insurance_date = veh_log['expiration_date']
-                rec.insurance_company = veh_log['insurer_id']['name']
+            if len(veh_ins_log) == 1:
+                rec.insurance_date = veh_ins_log['expiration_date']
+                rec.insurance_company = veh_ins_log['insurer_id']['name']
